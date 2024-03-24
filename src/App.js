619801fe75f2d15ec2fdef138be1b818c696b119
Jason Cain <118203097+cainjason@users.noreply.github.com>
2024-03-24 19:45:52 -0400

    chore(bootstrapped)tailwindcss

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import Poster from "./Components/Poster";
+
+function App() {
+  return (
+    <div className="app">
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <Poster />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Poster.js
@@ -0,0 +1,5 @@
+function Poster() {
+  return <header className="bg-green-200 h-3 w-full">Hello</header>;
+}
+
+export default Poster;
